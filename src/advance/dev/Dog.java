7878949fd2f3ea22ad726cf2a925a2278a25c952
luongminhduong <Duong40541@gmail.com>
2021-06-07 21:42:43 +0700

    first commit

---
+++ src/advance/dev/Dog.java
@@ -0,0 +1,18 @@
+package advance.dev;
+
+public class Dog extends Animal implements Runable{
+
+	@Override
+	void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ src/advance/dev/Fish.java
@@ -0,0 +1,13 @@
+package advance.dev;
+
+public class Fish extends Animal {
+
+	@Override
+	void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+}
